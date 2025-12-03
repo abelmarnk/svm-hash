c08c7121cd787016185d1598ad6bc35efafc8a47
Abel Marnk <orinayobanji@gmail.com>
2025-12-03 14:26:54 +0100

    Add more tests

--- test-program/src/lib.rs
+++ test-program/src/lib.rs
@@ -1,96 +1,66 @@
-#![no_std]
 use core::{
-    hash::{
-        Hash,
-        Hasher
-    }, 
-    ops::{
-        Sub,
-        Add
-    }
+    hash::{Hash, Hasher},
+    hint,
+    ops::{Add, Sub},
 };
-
-use hasher::{
-    SvmSHA256Hasher
-};
-
 use pinocchio::{
-    ProgramResult, account_info::AccountInfo, 
-    entrypoint, nostd_panic_handler, 
-    program_error::ProgramError, 
-    pubkey::{
-        Pubkey
-    }, 
-    syscalls::sol_remaining_compute_units
+    account_info::AccountInfo,
+    entrypoint,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    syscalls::{sol_remaining_compute_units, sol_set_return_data},
+    ProgramResult,
 };
-
-use pinocchio_pubkey::{
-    declare_id
-};
+use pinocchio_log::log;
+use pinocchio_pubkey::declare_id;
+use std::collections::{hash_map::DefaultHasher, HashMap, HashSet};
+use svm_hasher::SvmSHA256Hasher;
+use svm_hashmap::HashMap as SvmHashMap;
+use svm_hashset::HashSet as SvmHashSet;
 
 declare_id!("6q9CxFWZUyGnY3qWajwYHPLE5XMRhr7JYbfrxtnLB6Zw");
 
-use pinocchio_log::{
-    log
-};
-
-use siphasher::sip::{
-    SipHasher13
-};
-
-use svm_hashmap::HashMap;
-
-use svm_hashset::HashSet;
-
 entrypoint!(processor);
-nostd_panic_handler!();
-
-
-extern crate alloc;
 
 pub fn processor(
-    _program_id:&Pubkey,
-    _accounts:&[AccountInfo],
-    instruction_data:&[u8]
-)->ProgramResult{
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
     match instruction_data[0] {
-        0 => {
-            compare_cu_from_hash(&instruction_data[1..])
-        },
-        1 => {
-            test_hash_map()
-        },
-        2 => {
-            test_hash_set()
-        }
+        0 => test_hash_map(),
+        1 => test_hash_set(),
+        2 => compare_cu_from_hash(&instruction_data[1..]),
+        3 => compare_cu_from_hash_set(&instruction_data[1..]),
+        4 => compare_cu_from_hash_map(&instruction_data[1..]),
+        5 => compare_cu_from_all(&instruction_data[1..]),
         _ => {
             return Err(ProgramError::InvalidInstructionData);
         }
     }
 }
 
-pub fn test_hash_set()->ProgramResult{
-
+pub fn test_hash_set() -> ProgramResult {
     // `from` calls `from_iter`
-    let hashset = HashSet::from([
-        Pubkey::from([0;32]),
-        Pubkey::from([1;32]),
-        Pubkey::from([2;32]),
-        Pubkey::from([3;32])
+    let hashset = SvmHashSet::from([
+        Pubkey::from([0; 32]),
+        Pubkey::from([1; 32]),
+        Pubkey::from([2; 32]),
+        Pubkey::from([3; 32]),
     ]);
 
     // `deref`
     if !hashset.contains(&Pubkey::from([0; 32]))
         || !hashset.contains(&Pubkey::from([1; 32]))
         || !hashset.contains(&Pubkey::from([2; 32]))
-        || !hashset.contains(&Pubkey::from([3; 32])){
-        
+        || !hashset.contains(&Pubkey::from([3; 32]))
+    {
         log!("Fail!!!");
         return Err(Error::Fail.into());
     }
-    
+
     // `new`
-    let mut hashset = HashSet::new();
+    let mut hashset = SvmHashSet::new();
 
     // `deref_mut`
     hashset.insert(Pubkey::from([0; 32]));
@@ -101,14 +71,14 @@ pub fn test_hash_set()->ProgramResult{
     if !hashset.contains(&Pubkey::from([0; 32]))
         || !hashset.contains(&Pubkey::from([1; 32]))
         || !hashset.contains(&Pubkey::from([2; 32]))
-        || !hashset.contains(&Pubkey::from([3; 32])){
-            
-            log!("Fail!!!");
-            return Err(Error::Fail.into());
+        || !hashset.contains(&Pubkey::from([3; 32]))
+    {
+        log!("Fail!!!");
+        return Err(Error::Fail.into());
     }
 
     // `with_capacity`
-    let mut hashset = HashSet::with_capacity(4);
+    let mut hashset = SvmHashSet::with_capacity(4);
 
     hashset.insert(Pubkey::from([0; 32]));
     hashset.insert(Pubkey::from([1; 32]));
@@ -118,10 +88,10 @@ pub fn test_hash_set()->ProgramResult{
     if !hashset.contains(&Pubkey::from([0; 32]))
         || !hashset.contains(&Pubkey::from([1; 32]))
         || !hashset.contains(&Pubkey::from([2; 32]))
-        || !hashset.contains(&Pubkey::from([3; 32])){
-            
-            log!("Fail!!!");
-            return Err(Error::Fail.into());
+        || !hashset.contains(&Pubkey::from([3; 32]))
+    {
+        log!("Fail!!!");
+        return Err(Error::Fail.into());
     }
 
     Ok(())
@@ -129,7 +99,7 @@ pub fn test_hash_set()->ProgramResult{
 
 pub fn test_hash_map() -> ProgramResult {
     // `from` calls `from_iter`
-    let hashmap = HashMap::from([
+    let hashmap = SvmHashMap::from([
         (Pubkey::from([4; 32]), 4),
         (Pubkey::from([5; 32]), 5),
         (Pubkey::from([6; 32]), 6),
@@ -147,7 +117,7 @@ pub fn test_hash_map() -> ProgramResult {
     }
 
     // `new`
-    let mut hashmap = HashMap::new();
+    let mut hashmap = SvmHashMap::new();
 
     // `deref_mut`
     hashmap.insert(Pubkey::from([4; 32]), 4);
@@ -165,7 +135,7 @@ pub fn test_hash_map() -> ProgramResult {
     }
 
     // `with_capacity`
-    let mut hashmap = HashMap::with_capacity(4);
+    let mut hashmap = SvmHashMap::with_capacity(4);
 
     hashmap.insert(Pubkey::from([4; 32]), 4);
     hashmap.insert(Pubkey::from([5; 32]), 5);
@@ -184,76 +154,287 @@ pub fn test_hash_map() -> ProgramResult {
     Ok(())
 }
 
-pub fn compare_cu_from_hash(
-    data:&[u8]
-)->ProgramResult{
-
-    let mut siphasher = SipHasher13::new();
+pub fn compare_cu_from_hash(data: &[u8]) -> ProgramResult {
+    let mut default_hasher = DefaultHasher::new();
     let mut custom_hasher = SvmSHA256Hasher::default();
 
     let remaining_compute_units_1 = remaining_compute_units();
-    
-    data.hash(&mut siphasher);
 
-    let siphasher_result = siphasher.finish();
+    data.hash(&mut default_hasher);
+
+    let _ = hint::black_box(default_hasher.finish());
 
     let remaining_compute_units_2 = remaining_compute_units();
 
-    // custom_hasher.write(data);
     data.hash(&mut custom_hasher);
 
-    let custom_hasher_result = custom_hasher.finish();
+    let _ = hint::black_box(custom_hasher.finish());
 
     let remaining_compute_units_3 = remaining_compute_units();
 
-    let remaining_compute_units_compute_units = 
-        get_remaining_compute_units_compute_units();
+    let remaining_compute_units_compute_units = get_remaining_compute_units_compute_units();
 
-    log!("Remaining compute units compute unitss: {}", remaining_compute_units_compute_units);
+    let default_hasher_compute_units = remaining_compute_units_1
+        .sub(remaining_compute_units_2.add(remaining_compute_units_compute_units));
 
-    let siphasher_compute_units = remaining_compute_units_1.sub(remaining_compute_units_2.
-            add(remaining_compute_units_compute_units));
+    let custom_hasher_compute_units = remaining_compute_units_2
+        .sub(remaining_compute_units_3.add(remaining_compute_units_compute_units));
 
-    log!("Siphasher compute units: {}", siphasher_compute_units);
+    let return_data: Vec<u8> = [
+        default_hasher_compute_units.to_le_bytes().as_ref(),
+        custom_hasher_compute_units.to_le_bytes().as_ref(),
+    ]
+    .concat();
 
-    let custom_hasher_compute_units = remaining_compute_units_2.sub(remaining_compute_units_3.
-            add(remaining_compute_units_compute_units));
-
-    log!("Custom hasher compute units compute units: {}", custom_hasher_compute_units);
-
-    log!("siphasher result: {}", siphasher_result);
-
-    log!("Custom hasher result: {}", custom_hasher_result);
+    unsafe {
+        sol_set_return_data(
+            return_data.as_ptr(),
+            u64::try_from(return_data.len()).unwrap(),
+        );
+    }
 
     Ok(())
 }
 
-#[inline(always)]
-pub fn get_remaining_compute_units_compute_units()->u64{
+pub fn compare_cu_from_hash_set(data: &[u8]) -> ProgramResult {
+    let data = into_slices(data);
 
+    let mut default_hasher_set = HashSet::with_capacity(data.len());
+    default_hasher_set.extend(data.iter().map(|(first, _)| *first).into_iter());
+
+    let mut custom_hasher_set = SvmHashSet::with_capacity(data.len());
+    custom_hasher_set.extend(data.iter().map(|(first, _)| *first).into_iter());
+
+    let mut return_data = Vec::with_capacity(data.len());
+
+    for data in data {
+        let remaining_compute_units_1 = remaining_compute_units();
+
+        let _ = hint::black_box(default_hasher_set.contains(data.0));
+
+        let remaining_compute_units_2 = remaining_compute_units();
+
+        let _ = hint::black_box(custom_hasher_set.contains(data.0));
+
+        let remaining_compute_units_3 = remaining_compute_units();
+
+        let remaining_compute_units_compute_units = get_remaining_compute_units_compute_units();
+
+        let default_hasher_compute_units = remaining_compute_units_1
+            .sub(remaining_compute_units_2.add(remaining_compute_units_compute_units));
+
+        let custom_hasher_compute_units = remaining_compute_units_2
+            .sub(remaining_compute_units_3.add(remaining_compute_units_compute_units));
+
+        return_data.extend_from_slice(default_hasher_compute_units.to_le_bytes().as_ref());
+        return_data.extend_from_slice(custom_hasher_compute_units.to_le_bytes().as_ref());
+    }
+
+    unsafe {
+        sol_set_return_data(
+            return_data.as_ptr(),
+            u64::try_from(return_data.len()).unwrap(),
+        );
+    }
+
+    Ok(())
+}
+
+pub fn compare_cu_from_hash_map(data: &[u8]) -> ProgramResult {
+    let data = into_slices(data);
+
+    let mut default_hasher_map = HashMap::with_capacity(data.len());
+    default_hasher_map.extend(
+        data.iter()
+            .map(|(first, second)| (*first, *second))
+            .into_iter(),
+    );
+
+    let mut custom_hasher_map = SvmHashMap::with_capacity(data.len());
+    custom_hasher_map.extend(
+        data.iter()
+            .map(|(first, second)| (*first, *second))
+            .into_iter(),
+    );
+
+    let mut return_data = Vec::with_capacity(data.len());
+
+    for data in data {
+        let remaining_compute_units_1 = remaining_compute_units();
+
+        let _ = hint::black_box(default_hasher_map[data.0]);
+
+        let remaining_compute_units_2 = remaining_compute_units();
+
+        let _ = hint::black_box(custom_hasher_map[data.0]);
+
+        let remaining_compute_units_3 = remaining_compute_units();
+
+        let remaining_compute_units_compute_units = get_remaining_compute_units_compute_units();
+
+        let default_hasher_compute_units = remaining_compute_units_1
+            .sub(remaining_compute_units_2.add(remaining_compute_units_compute_units));
+
+        let custom_hasher_compute_units = remaining_compute_units_2
+            .sub(remaining_compute_units_3.add(remaining_compute_units_compute_units));
+
+        return_data.extend_from_slice(default_hasher_compute_units.to_le_bytes().as_ref());
+        return_data.extend_from_slice(custom_hasher_compute_units.to_le_bytes().as_ref());
+    }
+
+    unsafe {
+        sol_set_return_data(
+            return_data.as_ptr(),
+            u64::try_from(return_data.len()).unwrap(),
+        );
+    }
+
+    Ok(())
+}
+
+pub fn compare_cu_from_all(data: &[u8]) -> ProgramResult {
+    let data = into_slices(data);
+
+    let mut default_hasher = DefaultHasher::new();
+    let mut custom_hasher = SvmSHA256Hasher::default();
+
+    let mut default_hasher_set = HashSet::with_capacity(data.len());
+    default_hasher_set.extend(data.iter().map(|(first, _)| *first).into_iter());
+
+    let mut custom_hasher_set = SvmHashSet::with_capacity(data.len());
+    custom_hasher_set.extend(data.iter().map(|(first, _)| *first).into_iter());
+
+    let mut default_hasher_map = HashMap::with_capacity(data.len());
+    default_hasher_map.extend(
+        data.iter()
+            .map(|(first, second)| (*first, *second))
+            .into_iter(),
+    );
+
+    let mut custom_hasher_map = SvmHashMap::with_capacity(data.len());
+    custom_hasher_map.extend(
+        data.iter()
+            .map(|(first, second)| (*first, *second))
+            .into_iter(),
+    );
+
+    let mut return_data = Vec::with_capacity(data.len() * 8 * 6);
+
+    for data in data {
+        // hasher
+        let remaining_compute_units_1 = remaining_compute_units();
+
+        data.hash(&mut default_hasher);
+
+        let _ = hint::black_box(default_hasher.finish());
+
+        let remaining_compute_units_2 = remaining_compute_units();
+
+        data.hash(&mut custom_hasher);
+
+        let _ = hint::black_box(custom_hasher.finish());
+
+        let remaining_compute_units_3 = remaining_compute_units();
+
+        let remaining_compute_units_compute_units = get_remaining_compute_units_compute_units();
+
+        let default_hasher_compute_units = remaining_compute_units_1
+            .sub(remaining_compute_units_2.add(remaining_compute_units_compute_units));
+
+        let custom_hasher_compute_units = remaining_compute_units_2
+            .sub(remaining_compute_units_3.add(remaining_compute_units_compute_units));
+
+        return_data.extend_from_slice(default_hasher_compute_units.to_le_bytes().as_ref());
+        return_data.extend_from_slice(custom_hasher_compute_units.to_le_bytes().as_ref());
+
+        // hashset
+        let remaining_compute_units_1 = remaining_compute_units();
+
+        let _ = hint::black_box(default_hasher_set.contains(data.0));
+
+        let remaining_compute_units_2 = remaining_compute_units();
+
+        let _ = hint::black_box(custom_hasher_set.contains(data.0));
+
+        let remaining_compute_units_3 = remaining_compute_units();
+
+        let remaining_compute_units_compute_units = get_remaining_compute_units_compute_units();
+
+        let default_hasher_compute_units = remaining_compute_units_1
+            .sub(remaining_compute_units_2.add(remaining_compute_units_compute_units));
+
+        let custom_hasher_compute_units = remaining_compute_units_2
+            .sub(remaining_compute_units_3.add(remaining_compute_units_compute_units));
+
+        return_data.extend_from_slice(default_hasher_compute_units.to_le_bytes().as_ref());
+        return_data.extend_from_slice(custom_hasher_compute_units.to_le_bytes().as_ref());
+
+        // hashmap
+        let remaining_compute_units_1 = remaining_compute_units();
+
+        let _ = hint::black_box(default_hasher_map[data.0]);
+
+        let remaining_compute_units_2 = remaining_compute_units();
+
+        let _ = hint::black_box(custom_hasher_map[data.0]);
+
+        let remaining_compute_units_3 = remaining_compute_units();
+
+        let remaining_compute_units_compute_units = get_remaining_compute_units_compute_units();
+
+        let default_hasher_compute_units = remaining_compute_units_1
+            .sub(remaining_compute_units_2.add(remaining_compute_units_compute_units));
+
+        let custom_hasher_compute_units = remaining_compute_units_2
+            .sub(remaining_compute_units_3.add(remaining_compute_units_compute_units));
+
+        return_data.extend_from_slice(default_hasher_compute_units.to_le_bytes().as_ref());
+        return_data.extend_from_slice(custom_hasher_compute_units.to_le_bytes().as_ref());
+    }
+
+    unsafe {
+        sol_set_return_data(
+            return_data.as_ptr(),
+            u64::try_from(return_data.len()).unwrap(),
+        );
+    }
+
+    Ok(())
+}
+
+fn into_slices(mut data: &[u8]) -> Vec<(&[u8], &[u8])> {
+    let mut result = vec![];
+
+    while !data.is_empty() {
+        let data_len = usize::from(data[0]);
+        let temp_data = &data[1..data_len];
+        result.push((temp_data, temp_data));
+        data = &data[(1 + data_len)..];
+    }
+
+    result
+}
+
+pub fn get_remaining_compute_units_compute_units() -> u64 {
     // sol_remaining_compute_units cost 100CUs but this would take into account
     // the CUs from any extra operations like assignment
     unsafe {
         let remaining_compute_units_1 = sol_remaining_compute_units();
-
         let remaining_compute_units_2 = sol_remaining_compute_units();
-
         remaining_compute_units_1.sub(remaining_compute_units_2)
     }
 }
 
 #[inline(always)]
-pub fn remaining_compute_units()->u64{
-    unsafe {
-        sol_remaining_compute_units()
-    }
+pub fn remaining_compute_units() -> u64 {
+    unsafe { sol_remaining_compute_units() }
 }
 
-pub enum Error{
-    Fail
+pub enum Error {
+    Fail,
 }
 
-impl From<Error> for ProgramError{
+impl From<Error> for ProgramError {
     fn from(value: Error) -> Self {
         ProgramError::Custom(value as u32)
     }
--- test-program/tests/test.rs
+++ test-program/tests/test.rs
@@ -1,78 +1,287 @@
-use solana_instruction::{
-    Instruction
-};
+use rand::Rng;
+use solana_instruction::Instruction;
 
-use solana_address::{
-    Address
-};
+use solana_address::Address;
 
 use test_program::ID as TEST_PROGRAM_ID;
 
-use mollusk_svm::{
-    Mollusk, result::Check
-};
+use mollusk_svm::{result::Check, Mollusk};
 
 #[test]
-pub fn test_compare_cu_from_hash(){
-
+#[ignore]
+pub fn test_hash_map() {
     let program_id = Address::from(TEST_PROGRAM_ID);
-    let mollusk = Mollusk::new(
-        &program_id, "test_program"
-    );
 
-    let compare_cu_from_hash_instruction = Instruction{
+    let mollusk = Mollusk::new(&program_id, "test_program");
+
+    let test_hash_map_instruction = Instruction {
         program_id,
-        accounts:vec![],
-        data:[
-            vec![0u8], 
-            (0..255).collect()
-        ].concat()
+        accounts: vec![],
+        data: vec![0],
     };
 
-    mollusk.process_and_validate_instruction(
-        &compare_cu_from_hash_instruction, 
-        &[], 
-        &[Check::success()]
-    );
+    mollusk.process_and_validate_instruction(&test_hash_map_instruction, &[], &[Check::success()]);
 }
 
 #[test]
-pub fn test_hash_map(){
+#[ignore]
+pub fn test_hash_set() {
     let program_id = Address::from(TEST_PROGRAM_ID);
+    let mollusk = Mollusk::new(&program_id, "test_program");
 
-    let mollusk = Mollusk::new(
-        &program_id, "test_program"
-    );
-
-    let test_hash_map_instruction = Instruction{
+    let test_hash_set_instruction = Instruction {
         program_id,
-        accounts:vec![],
-        data:vec![1]
+        accounts: vec![],
+        data: vec![1],
     };
 
-    mollusk.process_and_validate_instruction(
-        &test_hash_map_instruction, 
-        &[], 
-        &[Check::success()]
-    );
+    mollusk.process_and_validate_instruction(&test_hash_set_instruction, &[], &[Check::success()]);
+}
+
+const INPUT_COUNT: usize = 256;
+const INNER_INPUT_COUNT: usize = 1;
+const LOWER_BOUND: usize = 8;
+const UPPER_BOUND: usize = 255;
+
+fn random_input_data() -> Vec<u8> {
+    let mut rng = rand::rng();
+    let len = rng.random_range(LOWER_BOUND..=UPPER_BOUND);
+    (0..len).map(|_| rng.random()).collect()
 }
 
 #[test]
-pub fn test_hash_set(){
+fn test_compare_cu_from_hash() {
     let program_id = Address::from(TEST_PROGRAM_ID);
-    let mollusk = Mollusk::new(
-        &program_id, "test_program"
-    );
+    let mollusk = Mollusk::new(&program_id, "test_program");
 
-    let test_hash_set_instruction = Instruction{
-        program_id,
-        accounts:vec![],
-        data:vec![2]
-    };
+    let mut output = Vec::with_capacity(INPUT_COUNT);
 
-    mollusk.process_and_validate_instruction(
-        &test_hash_set_instruction, 
-        &[], 
-        &[Check::success()]
-    );
+    for run_count in 0..INPUT_COUNT {
+        let data = random_input_data();
+        let data_len = data.len();
+
+        let instruction = Instruction {
+            program_id,
+            data: std::iter::once(2u8)
+                .chain(data.into_iter())
+                .collect::<Vec<u8>>(),
+            accounts: vec![],
+        };
+
+        let result =
+            mollusk.process_and_validate_instruction(&instruction, &[], &[Check::success()]);
+
+        let (siphasher_compute_units, custom_compute_units) = (
+            u64::from_le_bytes(result.return_data[..8].try_into().unwrap()),
+            u64::from_le_bytes(result.return_data[8..].try_into().unwrap()),
+        );
+
+        output.push(format!(
+            "Run {}: \n\
+            Data length: {} \n\
+            Siphasher: {} | Custom: {} \n",
+            run_count, data_len, siphasher_compute_units, custom_compute_units
+        ));
+    }
+
+    for output in output {
+        println!("{}", output);
+    }
+}
+
+#[test]
+#[ignore]
+fn test_compare_cu_from_hash_set() {
+    let program_id = Address::from(TEST_PROGRAM_ID);
+    let mollusk = Mollusk::new(&program_id, "test_program");
+
+    let mut output = Vec::with_capacity(INPUT_COUNT);
+
+    for run_count in 0..INPUT_COUNT {
+        let mut data: Vec<_> = vec![];
+        let mut data_lens: Vec<_> = vec![];
+        for _ in 0..INNER_INPUT_COUNT {
+            let inner_data = random_input_data();
+            let inner_data_len = u8::try_from(inner_data.len()).unwrap();
+
+            data.push(inner_data_len);
+            data.extend_from_slice(inner_data.as_slice());
+
+            data_lens.push(inner_data_len);
+        }
+
+        let instruction = Instruction {
+            program_id,
+            data: std::iter::once(3u8)
+                .chain(data.into_iter())
+                .collect::<Vec<u8>>(),
+            accounts: vec![],
+        };
+
+        let result =
+            mollusk.process_and_validate_instruction(&instruction, &[], &[Check::success()]);
+
+        let mut return_data = result.return_data.as_slice();
+
+        output.push(format!("Run {}: \n", run_count));
+
+        for data_len in data_lens {
+            let (siphasher_compute_units, custom_compute_units) = (
+                u64::from_le_bytes(return_data[..8].try_into().unwrap()),
+                u64::from_le_bytes(return_data[8..16].try_into().unwrap()),
+            );
+
+            output.push(format!(
+                "Data length: {} \n\
+                Siphasher: {} | Custom: {} \n",
+                data_len, siphasher_compute_units, custom_compute_units
+            ));
+
+            return_data = &return_data[16..];
+        }
+    }
+
+    for output in output {
+        println!("{}", output);
+    }
+}
+
+#[test]
+#[ignore]
+fn test_compare_cu_from_hash_map() {
+    let program_id = Address::from(TEST_PROGRAM_ID);
+    let mollusk = Mollusk::new(&program_id, "test_program");
+
+    let mut output = Vec::with_capacity(INPUT_COUNT);
+
+    for run_count in 0..INPUT_COUNT {
+        let mut data: Vec<_> = vec![];
+        let mut data_lens: Vec<_> = vec![];
+        for _ in 0..INNER_INPUT_COUNT {
+            let inner_data = random_input_data();
+            let inner_data_len = u8::try_from(inner_data.len()).unwrap();
+
+            data.push(inner_data_len);
+            data.extend_from_slice(inner_data.as_slice());
+
+            data_lens.push(inner_data_len);
+        }
+
+        let instruction = Instruction {
+            program_id,
+            data: std::iter::once(4u8)
+                .chain(data.into_iter())
+                .collect::<Vec<u8>>(),
+            accounts: vec![],
+        };
+
+        let result =
+            mollusk.process_and_validate_instruction(&instruction, &[], &[Check::success()]);
+
+        let mut return_data = result.return_data.as_slice();
+
+        output.push(format!("Run {}: \n", run_count));
+
+        for data_len in data_lens {
+            let (siphasher_compute_units, custom_compute_units) = (
+                u64::from_le_bytes(return_data[..8].try_into().unwrap()),
+                u64::from_le_bytes(return_data[8..16].try_into().unwrap()),
+            );
+
+            output.push(format!(
+                "Data length: {} \n\
+                Siphasher: {} | Custom: {} \n",
+                data_len, siphasher_compute_units, custom_compute_units
+            ));
+
+            return_data = &return_data[16..];
+        }
+    }
+
+    for output in output {
+        println!("{}", output);
+    }
+}
+
+#[test]
+#[ignore]
+fn test_compare_cu_from_all() {
+    let program_id = Address::from(TEST_PROGRAM_ID);
+    let mollusk = Mollusk::new(&program_id, "test_program");
+
+    let mut output = Vec::with_capacity(INPUT_COUNT);
+
+    for run_count in 0..INPUT_COUNT {
+        let mut data: Vec<_> = vec![];
+        let mut data_lens: Vec<_> = vec![];
+        for _ in 0..INNER_INPUT_COUNT {
+            let inner_data = random_input_data();
+            let inner_data_len = u8::try_from(inner_data.len()).unwrap();
+
+            data.push(inner_data_len);
+            data.extend_from_slice(inner_data.as_slice());
+
+            data_lens.push(inner_data_len);
+        }
+
+        let instruction = Instruction {
+            program_id,
+            data: std::iter::once(5u8)
+                .chain(data.into_iter())
+                .collect::<Vec<u8>>(),
+            accounts: vec![],
+        };
+
+        let result =
+            mollusk.process_and_validate_instruction(&instruction, &[], &[Check::success()]);
+
+        let mut return_data = result.return_data.as_slice();
+
+        output.push(format!("Run {}: \n", run_count));
+
+        for data_len in data_lens {
+            let (siphasher_compute_units, custom_compute_units) = (
+                u64::from_le_bytes(return_data[..8].try_into().unwrap()),
+                u64::from_le_bytes(return_data[8..16].try_into().unwrap()),
+            );
+
+            output.push(format!(
+                "Data length: {} \n\
+                Siphasher: {} | Custom: {} \n",
+                data_len, siphasher_compute_units, custom_compute_units
+            ));
+
+            return_data = &return_data[16..];
+
+            let (siphasher_compute_units, custom_compute_units) = (
+                u64::from_le_bytes(return_data[..8].try_into().unwrap()),
+                u64::from_le_bytes(return_data[8..16].try_into().unwrap()),
+            );
+
+            output.push(format!(
+                "Data length: {} \n\
+                Siphasher set: {} | Custom set: {} \n",
+                data_len, siphasher_compute_units, custom_compute_units
+            ));
+
+            return_data = &return_data[16..];
+
+            let (siphasher_compute_units, custom_compute_units) = (
+                u64::from_le_bytes(return_data[..8].try_into().unwrap()),
+                u64::from_le_bytes(return_data[8..16].try_into().unwrap()),
+            );
+
+            output.push(format!(
+                "Data length: {} \n\
+                Siphasher map: {} | Custom map: {} \n",
+                data_len, siphasher_compute_units, custom_compute_units
+            ));
+
+            return_data = &return_data[16..];
+        }
+    }
+
+    for output in output {
+        println!("{}", output);
+    }
 }
